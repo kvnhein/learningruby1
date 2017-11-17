43c778dc230386ee9f64a39c0b3e0e44a9a99fa2
Kevin Hein <kevinhein819@gmail.com>
2017-11-17 01:53:27 +0000

    Added image uploads new

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
-    belongs_to :user
-    belongs_to :receiver, class_name: 'User'
-    default_scope { order(created_at: 'DESC') }
+  belongs_to :user
+  belongs_to :receiver, class_name: 'User'
+  default_scope { order(created_at: 'DESC') }
+  
+  has_attached_file :image, styles: { medium: "500x500>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
